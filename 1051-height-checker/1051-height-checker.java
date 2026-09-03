eb817a46032e4012b1662adcd3ad2d98744c7cc5
Dibya Ranjan Samal <122545058+Dibya0217@users.noreply.github.com>
2026-09-03 10:46:13 +0530

    Time: 4 ms (78.21%), Space: 43.6 MB (27.97%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] clone = heights.clone();
+        Arrays.sort(clone);
+        int count = 0;
+        for(int i = 0; i < heights.length; i++) {
+            if(heights[i] != clone[i]) count++;
+        }
+        return count;
+    }
+}
